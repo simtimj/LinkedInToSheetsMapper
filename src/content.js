b72965b7acb82ead9d492a1e8d1ec3f45441691d
Julius Simtim <juliussimtim@gmail.com>
2025-09-02 16:11:03 -0700

    Grab companyName, position, location, salaryRange, and jobLink

--- src/content.js
+++ src/content.js
@@ -1,11 +1,8 @@
 
 function scrapeJobs() {
   let companyClass = document.querySelectorAll("job-details-jobs-unified-top-card__company-name")
-  let positionClass;
-  let locationClass;
-  let salaryRangeClass;
-  let linkClass;
   
+
   // Example: scrape job titles from a page
   const jobs = [...document.querySelectorAll(".job-card-title")]
     .map(el => el.innerText.trim());
--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,6 @@
 // document.querySelector("h1");          // first <h1>
 // document.querySelectorAll("")
 
-
 // steps:
 // 1. add button in popup.html
 // 2. add event listener in popup.js
@@ -10,18 +9,40 @@
 // document.getElementsByClassName("job-details-jobs-unified-top-card__company-name")
 
 document.getElementById("submit").addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true }); // get active tab
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  // Inject content script to scrape the page
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: () => {
-      const companyClass = document.querySelector(".job-details-jobs-unified-top-card__company-name").
-      alert("companyClass:", JSON.stringify(companyClass));
-      return companyClass;
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      func: scrapePage,
+    },
+    (results) => {
+      const output = results[0].result;
+      document.getElementById("output").textContent = JSON.stringify(output, null, 2);
     }
-  }, (results) => {
-    alert(results)
-    alert("completed")
-  });
+  );
 });
+
+// This function runs inside the webpage
+function scrapePage() {
+  // Example: grab all links inside <div>
+  const companyName = Array.from(document.getElementsByClassName("job-details-jobs-unified-top-card__company-name")).map(a => (a.innerText.trim()))[0];
+  const positionClass = Array.from(document.getElementsByClassName("job-details-jobs-unified-top-card__job-title")).map(a => (a.innerText.trim()))[0];
+
+  let jobDetailsFitLevelPrefs = Array.from(document.getElementsByClassName("job-details-fit-level-preferences")).map(a => (a.innerText.trim()).split("\n"));
+
+  let salaryRangeClass = jobDetailsFitLevelPrefs[0][0];
+  let locationClass = jobDetailsFitLevelPrefs[0][1].slice(1);
+
+
+  let currTabUrl = window.location.href;
+  let jobID = currTabUrl.split("currentJobId=")[1].split("&")[0];
+
+  // get curent tab url
+  // parse  to resemble  https://www.linkedin.com/jobs/view/4294095087
+  let jobLink = `https://www.linkedin.com/jobs/view/${jobID}`;
+
+
+
+  return { companyName, positionClass, locationClass, salaryRangeClass, jobLink };
+}
